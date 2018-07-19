9db549b0b455c4634bc60c68bc87d9d2f34f1798
wfshan <wfshan06@gmail.com>
2018-07-19 23:25:38 +0800

    Create mapcoordTransform.py

---
+++ mapcoordTransform.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib
+import math
+from math import radians, cos, sin, asin, sqrt
+
+x_pi = 3.14159265358979324 * 3000.0 / 180.0
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+ee = 0.00669342162296594323  # 扁率
+
+
+class Geocoding:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def geocode(self, address):
+        """
+        利用高德geocoding服务解析地址获取位置坐标
+        :param address:需要解析的地址
+        :return:
+        """
+        geocoding = {'s': 'rsv3',
+                     'key': self.api_key,
+                     'city': '全国',
+                     'address': address}
+        geocoding = urllib.urlencode(geocoding)
+        ret = urllib.urlopen("%s?%s" % ("http://restapi.amap.com/v3/geocode/geo", geocoding))
+
+        if ret.getcode() == 200:
+            res = ret.read()
+            json_obj = json.loads(res)
+            if json_obj['status'] == '1' and int(json_obj['count']) >= 1:
+                geocodes = json_obj['geocodes'][0]
+                lng = float(geocodes.get('location').split(',')[0])
+                lat = float(geocodes.get('location').split(',')[1])
+                return [lng, lat]
+            else:
+                return None
+        else:
+            return None
+
+
+def gcj02_to_bd09(lng, lat):
+    """
+    火星坐标系(GCJ-02)转百度坐标系(BD-09)
+    谷歌、高德——>百度
+    :param lng:火星坐标经度
+    :param lat:火星坐标纬度
+    :return:
+    """
+    z = math.sqrt(lng * lng + lat * lat) + 0.00002 * math.sin(lat * x_pi)
+    theta = math.atan2(lat, lng) + 0.000003 * math.cos(lng * x_pi)
+    bd_lng = z * math.cos(theta) + 0.0065
+    bd_lat = z * math.sin(theta) + 0.006
+    return [bd_lng, bd_lat]
+
+
+def bd09_to_gcj02(bd_lon, bd_lat):
+    """
+    百度坐标系(BD-09)转火星坐标系(GCJ-02)
+    百度——>谷歌、高德
+    :param bd_lat:百度坐标纬度
+    :param bd_lon:百度坐标经度
+    :return:转换后的坐标列表形式
+    """
+    x = bd_lon - 0.0065
+    y = bd_lat - 0.006
+    z = math.sqrt(x * x + y * y) - 0.00002 * math.sin(y * x_pi)
+    theta = math.atan2(y, x) - 0.000003 * math.cos(x * x_pi)
+    gg_lng = z * math.cos(theta)
+    gg_lat = z * math.sin(theta)
+    return [gg_lng, gg_lat]
+
+
+
+def wgs84_to_gcj02(lng, lat):
+    """
+    WGS84转GCJ02(火星坐标系)
+    :param lng:WGS84坐标系的经度
+    :param lat:WGS84坐标系的纬度
+    :return:
+    """
+    if out_of_china(lng, lat):  # 判断是否在国内
+        return lng, lat
+    dlat = _transformlat(lng - 105.0, lat - 35.0)
+    dlng = _transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [mglng, mglat]
+
+
+def gcj02_to_wgs84(lng, lat):
+    """
+    GCJ02(火星坐标系)转GPS84
+    :param lng:火星坐标系的经度
+    :param lat:火星坐标系纬度
+    :return:
+    """
+    if out_of_china(lng, lat):
+        return lng, lat
+    dlat = _transformlat(lng - 105.0, lat - 35.0)
+    dlng = _transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [lng * 2 - mglng, lat * 2 - mglat]
+
+
+def bd09_to_wgs84(bd_lon, bd_lat):
+    lon, lat = bd09_to_gcj02(bd_lon, bd_lat)
+    return gcj02_to_wgs84(lon, lat)
+
+
+def wgs84_to_bd09(lon, lat):
+    lon, lat = wgs84_to_gcj02(lon, lat)
+    return gcj02_to_bd09(lon, lat)
+
+
+def _transformlat(lng, lat):
+    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+          0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lat * pi) + 40.0 *
+            math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 *
+            math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+
+def _transformlng(lng, lat):
+    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+          0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lng * pi) + 40.0 *
+            math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 *
+            math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+
+def latLng2WebMercator(lng, lat):
+    earthRad = 6378137.0
+    x = lng * math.pi / 180 * earthRad
+    a = lat * math.pi / 180
+    y = earthRad / 2 * math.log((1.0 + math.sin(a)) / (1.0 - math.sin(a)))
+    return [x, y]
+
+
+def webMercator2LngLat(x, y):
+    lng = x / 20037508.34 * 180
+    lat = y / 20037508.34 * 180
+    lat = 180 / math.pi * (2 * math.atan(math.exp(lat * math.pi / 180)) - math.pi / 2)
+    return [lng, lat]
+
+def haversine(lon1, lat1, lon2, lat2):  # 经度1，纬度1，经度2，纬度2 （十进制度数）
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # 将十进制度数转化为弧度
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine公式
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # 地球平均半径，单位为公里
+    return c * r * 1000
+
+def out_of_china(lng, lat):
+    """
+    判断是否在国内，不在国内不做偏移
+    :param lng:
+    :param lat:
+    :return:
+    """
+    return not (lng > 73.66 and lng < 135.05 and lat > 3.86 and lat < 53.55)
+
+def grid_to_wgs(lon,lat):
+    """
+    shly项目坐标回转
+    """
+    slon = 120.85680492
+    slat= 30.67559298
+    mlon= 0.002653328470790916
+    mlat= 0.0022544689779550493
+    wgslon = lon * mlon + slon + 0.5*mlon
+    wgslat = lat * mlat + slat + 0.5*mlat
+    
+    return [wgslon,wgslat]
+
+if __name__ == '__main__':
+    #31.184606, 121.438856,
+    #31.194182, 121.451828
+
+    #121.43245958105776,31.17827497124815,121.4454291830918,31.18783463517773
+
+
+    '''
+    29.260726
+    106.25746
+    '''
+
+    '''
+    30.161736
+    106.989147
+    '''
+
+    #121.444401,31.186417
+    # result1 = gcj02_to_bd09(116.469271,39.985568)
+    result2 = bd09_to_gcj02(106.989147, 30.161736)
+    # result3 = wgs84_to_gcj02(lng, lat)
+    # result4 = gcj02_to_wgs84(lng, lat)
+    # result5 = bd09_to_wgs84(lng, lat)
+    # result6 = wgs84_to_bd09(lng, lat)
+
+    # g = Geocoding('Api_KEY')  # 这里填写你的高德api的key
+    # result7 = g.geocode('北京市朝阳区朝阳公园')
+    print(result2)
+
+    '''
+    106.25090057986931, 29.25501485457217
+    106.9825390116396, 30.156048686963462
+
+    '''
